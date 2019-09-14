88d0bf8d856ab134e4d37bea341fe7aed3130f46
Greg Thomas <SilentGrig@gmail.com>
2019-09-14 14:44:02 +0100

    Updates

---
+++ precache-manifest.2d04d61441a9ee4f0527d2f84bd42bd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef784d9e184bd8836f58e581517ec8aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c03cc1390907a16fce07",
+    "url": "/static/css/main.642bf16f.chunk.css"
+  },
+  {
+    "revision": "2f0a062f2ddfc97bbcf9",
+    "url": "/static/js/2.d3c6c1cd.chunk.js"
+  },
+  {
+    "revision": "c03cc1390907a16fce07",
+    "url": "/static/js/main.3227e429.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
